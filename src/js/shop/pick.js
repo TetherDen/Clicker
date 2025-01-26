f42687dccb4b569b24dad99dd787ac3c933d1b5f
Denis <bigdramashow730@gmail.com>
2025-01-26 21:41:59 +0200

    PickIncome

--- src/js/shop/pick.js
+++ src/js/shop/pick.js
@@ -3,25 +3,26 @@ const pickBtn = document.getElementById("pick_btn");
 const priceElement = pickBtn.querySelector(".store__product_price");
 const levelElement = pickBtn.querySelector(".store__product_count");
 
-// TODO заменить счет игрока на реальный...
-let PlayerScore = 1000;     // <<-- Заглушка
+const PlayerScore = document.getElementById("score_points");
 
 
 //  TODO  подумать как лучше хранить переменные и потом сохранять/загружать в localStorage
 let basePickPrice = 10;
 let pickPriceMultiplier = 1.15;
 let pickBaseIncome = 1;
-let currentPickLevel = 1;
+let currentPickLevel = 0;
 
 // PickAxe Click Event
 pickBtn.addEventListener("click", () =>{
     const pickPrice = calculatePickPrice();
-    if(PlayerScore >= pickPrice) {
-        PlayerScore -= pickPrice;
+    const currentScore = getPlayerScore();
+    if(currentScore >= pickPrice) {
+        setPlayerScore(currentScore - pickPrice);
         currentPickLevel++;
-        // TODO Pick Income ?!
 
         updatePickDisplay();
+        //..... save mthod()
+
         console.log(`Pick upgraded to level: ${currentPickLevel}, Remaining score: ${PlayerScore}`);        // После тестов log убрать
     }
     else {
@@ -32,18 +33,39 @@ pickBtn.addEventListener("click", () =>{
 
 // Вызываем метод для обновления данных кирки
 updatePickDisplay();
+// Старт Доход Кирка в 1/сек
+startPickIncome();
+
 
 
 
 
 // Functions
+function startPickIncome() {
+    setInterval(() => {
+        const currentScore = getPlayerScore();
+        const income = pickBaseIncome * currentPickLevel;
+        setPlayerScore(currentScore + income);      
+    }, 1000);
+}
+
+
 function calculatePickPrice() {
-    return Math.floor(basePickPrice * Math.pow(pickPriceMultiplier, currentPickLevel - 1));
+    return Math.floor(basePickPrice * Math.pow(pickPriceMultiplier, currentPickLevel));
 }
 
 function updatePickDisplay() {
     priceElement.textContent = calculatePickPrice();
     levelElement.textContent = currentPickLevel;
+}
+
+function getPlayerScore() {
+    return parseInt(PlayerScore.textContent);
+}
+
+// обновление счета
+function setPlayerScore(newScore) {
+    PlayerScore.textContent = newScore;
 }
 
 
