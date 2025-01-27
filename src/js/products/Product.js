e758f4d5e5486cc6279459c43cac15c3a6167a4c
Ivan Ushachov <ushachovg324@gmail.com>
2025-01-27 15:46:32 +0200

    feat: updated js

---
+++ src/js/products/Product.js
@@ -0,0 +1,40 @@
+import {Game} from "../Game.js";
+
+export class Product {
+    priceElement = null;
+    levelElement = null;
+
+    constructor(img, name, income, price, multiplier, level) {
+        this.img = img;
+        this.name = name;
+        this.income = income;
+        this.price = price;
+        this.multiplier = multiplier;
+        this.level = level;
+    }
+
+    calculatePrice() {
+        return this.price *= this.multiplier.toFixed(1);
+    }
+
+    updateDisplay() {
+        if (this.priceElement && this.levelElement) {
+            this.priceElement.textContent = this.price.toFixed(1);
+            this.levelElement.textContent = this.level;
+        }
+    }
+
+    buy() {
+        const score = Game.getScore();
+
+        if (score >= this.price) {
+            Game.setScore(score - this.price)
+            this.level += 1;
+            this.price = this.calculatePrice();
+            this.updateDisplay();
+            return true;
+        }
+
+        return false;
+    }
+}
---
+++ src/js/products/Tnt.js
@@ -0,0 +1,15 @@
+import { Product } from "./Product.js";
+import { Game } from "../Game.js";
+export class Tnt extends Product {
+    constructor(img, name, income, price, multiplier, level) {
+        super(img, name, income, price, multiplier, level);
+    }
+
+    buy() {
+        if (super.buy()){
+            setInterval(() => {
+                Game.setScore(Game.getScore() + this.income);
+            }, 5000);
+        }
+    }
+}
