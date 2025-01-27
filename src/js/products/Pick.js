c1b326a1dff696e62ef8395831edac3ecdea5b81
Ivan Ushachov <ushachovg324@gmail.com>
2025-01-27 17:33:19 +0200

    feat: updated products

--- src/js/products/Pick.js
+++ src/js/products/Pick.js
@@ -2,15 +2,18 @@ import {Product} from "./Product.js";
 import {Game} from "../Game.js";
 
 export class Pick extends Product {
+    static isBought = false;
     constructor(img, name, income, price, multiplier, level) {
         super(img, name, income, price, multiplier, level);
     }
 
     buy() {
-        if (super.buy()) {
+        if (super.buy() && !this.isBought) {
             setInterval(() => {
-                Game.setScore(Game.getScore() + this.income);
+                Game.setScore(Game.getScore() + (this.income * this.level));
             }, 1000);
+
+            this.isBought = true;
         }
     }
 }
