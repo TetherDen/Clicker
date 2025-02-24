1e9e9f2b9e7b4007231c2ecb873b27516dbffe1b
Ivan Ushachov <ushachovg324@gmail.com>
2025-02-24 09:17:06 +0200

    updated products

--- src/js/products/Pick.js
+++ src/js/products/Pick.js
@@ -6,7 +6,6 @@ export class Pick extends Product {
     constructor(img, name, income, price, multiplier, level) {
         super(img, name, income, price, multiplier, level);
 
-        this.farmInterval = 1000;
         this.farmCallBack = () => {
             Game.setScore(Game.getScore() + (this.income * this.level))
         };
